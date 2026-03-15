ef2529517651ab5e601c1d64b99d238fee3f8248
Vikram Pal <vikrampal803302@gmail.com>
2026-03-15 17:51:06 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,5 @@
+n = int(input("number of rows enter your:"))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print("5", end=" ")
+    print()
---
+++ loop.py
@@ -0,0 +1,8 @@
+n = int(input("enter your fast"))
+n = int(input("enter your sec"))
+n = int(input("enter your th"))
+n = int(input("enter your for"))
+for i in range(1, n+1):
+    for j in range(1,n+1):
+        print("*", end=" ")
+    print()
