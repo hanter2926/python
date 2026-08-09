b5bc1005541068f3a69eb7bf23e0edf48b78732e
hanter2926 <vikrampal505@yahoo.com>
2026-08-09 23:38:37 +0530

    Add JSON serialization examples in new lages.py and string.py files

---
+++ function/json/lages.py
@@ -0,0 +1,12 @@
+import json
+x = {
+   " name" : "vikram pal",
+    "age" : 20,
+    "city" : "patana",
+    "married" : False,
+    "girlfriend" : True,
+    "friends" : ("aditya", "prsool", "Rambhol", "Rahul", "vikash", "ravi", "pardip"),
+    "pats" : None,
+    "bike" : True,
+}
+print(x)
