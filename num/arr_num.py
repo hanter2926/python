aab7818ff440588edfd10292ea843f2acdaa27bf
hanter2926 <vikrampal505@yahoo.com>
2026-08-12 21:52:19 +0530

    Fix array initialization and restore print statements in arr_num.py

--- num/arr_num.py
+++ num/arr_num.py
@@ -1,6 +1,11 @@
 import numpy as np 
 
-arr = np.array([20, 30, 50, 10])
+# arr = np.array([20, 30, 50, 10])
+
+# print(arr)
+# print(type(arr))
+
+arr = np.array([80, 0 0, 5.0, 0.1])
 
 print(arr)
 print(type(arr))
