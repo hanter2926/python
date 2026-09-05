7d4a608cbd6762c6623cdad24742278c24f249c9
hanter2926 <vikrampal505@yahoo.com>
2026-09-05 21:54:56 +0530

    Add second square function for addition operation in squari.py

--- function/Parameters/squari.py
+++ function/Parameters/squari.py
@@ -5,3 +5,10 @@ def square(x):
 result = square(5)
 print("result =", result)
 print("square + 10 =", result + 10)
+
+def square(a):
+    return a + a
+
+result = square(4)
+print ("result =", result)
+print("add + 6 =", result + 6)
