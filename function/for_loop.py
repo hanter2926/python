2eea72bd1b8f017d592d332b22022cc3ba27730b
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 22:46:38 +0530

    Add for loop examples to demonstrate number printing and summation

--- function/for_loop.py
+++ function/for_loop.py
@@ -18,4 +18,16 @@ print("\nrange(3,9,2):")
 for i in range(3,9,2):
     print(i)
 
+
+#print number for 1 to 10 with for loop
+
+
+for i in range(1,11):
+    print(i)
+
+#sum of Numbber using for loop Code 
+total = 0
+for i in range(1,11):
+    total += i
+    print("sum of number from 1 to 10 is:", total)
     
