d86a0bf1822ecd72dc1dcd4d625df7c8a21eaefb
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 22:39:14 +0530

    Add additional range examples to for_loop.py

--- function/for_loop.py
+++ function/for_loop.py
@@ -4,4 +4,18 @@ for i in range (10,15):
 fruits = ["apple", "banana", "cherry", "kiwi", "mango", "orange", "papaya", "pear", "plum", "grapes", "gauva", "coconut", "custard apple", "cucumber" ]
 for fruits in fruits:
     print(fruits)
+
+
+print("range(9):")
+for i in range(9):
+    print(i)
+
+print("\nrange(3,9):")
+for i in range(3,9):
+    print(i)
+
+print("\nrange(3,9,2):")
+for i in range(3,9,2):
+    print(i)
+
     
