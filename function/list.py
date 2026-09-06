23934b9d9c6aa1aca6f1944e3985d34fa1ee336b
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 23:07:02 +0530

    Fix formatting and add string iteration example in list.py

--- function/list.py
+++ function/list.py
@@ -1,5 +1,13 @@
-#loop thorugh list using for loop
+#loop Thorugh list using for loop
 
 students = ["Vikram" "Aditya", "Rohit", "Kamal", "Amit", "Naveen", "Veeke", "Vikash", "Ramanujan", "Suraj" ]
 for student in students:
     print(student)
+
+
+#loop Thorugh a String using for Loop
+
+
+word = "CPYTHON"
+for character in word:
+  print(character)
