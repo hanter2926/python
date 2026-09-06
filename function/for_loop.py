78a2751397449f155b0f9e41897523989c22df86
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 22:55:04 +0530

    Add list.py to demonstrate looping through a list of students

--- function/for_loop.py
+++ function/for_loop.py
@@ -30,4 +30,5 @@ total = 0
 for i in range(1,11):
     total += i
     print("sum of number from 1 to 10 is:", total)
-    
+
+
---
+++ function/list.py
@@ -0,0 +1,5 @@
+#loop thorugh list using for loop
+
+students = ["Vikram" "Aditya", "Rohit", "Kamal", "Amit", "Naveen", "Veeke", "Vikash", "Ramanujan", "Suraj" ]
+for student in students:
+    print(student)
