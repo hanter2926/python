df87bf2022824aeb1b2baadd2931582f9f917dd7
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 23:22:13 +0530

    Add loop.py to demonstrate for and while loop functionality

---
+++ function/loop.py
@@ -0,0 +1,11 @@
+# for VS while loop
+
+for i in range(1,15):
+    print(i)
+
+i = 1
+while i < 15:
+    print(i)
+i += 1
+
+# this code not for using while loop because it will run infinite time and we can't stop it.
