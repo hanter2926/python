777ecf289b0bb9b0eff560da14b7ee7364ac8b4c
hanter2926 <vikrampal505@yahoo.com>
2026-09-06 22:23:37 +0530

    Add for_loop.py to demonstrate basic for loop and fruit iteration

---
+++ function/for_loop.py
@@ -0,0 +1,7 @@
+for i in range (10,15):
+    print(i)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango", "orange", "papaya", "pear", "plum", "grapes", "gauva", "coconut", "custard apple", "cucumber" ]
+for fruits in fruits:
+    print(fruits)
+    
